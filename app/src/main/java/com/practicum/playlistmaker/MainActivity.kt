a8d9a880d68ba99523968a86defd4dbada64643a
lemurka888 <Wuniimura@yandex.ru>
2026-07-14 03:26:38 -0700

    переход на страницы по кнопкам

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,8 +13,27 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
+
+        val btnSearch = findViewById<Button>(R.id.btnSearch)
+        val btnMedia = findViewById<Button>(R.id.btnMedia)
+        val btnSetting = findViewById<Button>(R.id.btnSetting)
+
+        btnMedia.setOnClickListener {
+            val mediaIntent = Intent(this, Mediateka::class.java)
+            startActivity(mediaIntent)
+        }
+
+        btnSetting.setOnClickListener {
+            val settingIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingIntent)
+        }
+
+
+        btnSearch.setOnClickListener {
+            val searchIntent = Intent(this, SearchActivity::class.java)
+            startActivity(searchIntent)
+        }
 
         }
     }
